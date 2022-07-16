71e3a3a1b38a50b27f68b0f6ad8503312738e026
Dor Shinar <dors718@gmail.com>
2022-07-16 15:01:56 +0300

    WIP global FormData

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,19 @@
+import { vi } from "vitest";
+
 export default async function setup() {
+  console.log(process.version);
+
   if (!process.version.startsWith("v18")) {
-    global.FormData = (await import("formdata-polyfill")).FormData;
+    console.log("adding FormData to global");
+
+    const { FormData: _FormData } = await import("formdata-polyfill/esm.min");
+    global.FormData = _FormData;
+    globalThis.FormData = _FormData;
+
+    vi.stubGlobal("FormData", _FormData);
+
+    const fd = new _FormData();
+
+    console.log(FormData, global.FormData, fd);
   }
 }
